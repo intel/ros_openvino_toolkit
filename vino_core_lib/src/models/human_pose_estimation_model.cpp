041534e93abf3b9e1c85be0f0b81bbf341554e7c
Silas Alves <silas.alves@gmail.com>
2020-04-17 14:46:54 -0400

    Adds missing model verification.

--- vino_core_lib/src/models/human_pose_estimation_model.cpp
+++ vino_core_lib/src/models/human_pose_estimation_model.cpp
@@ -1,25 +1,11 @@
 // TODO add license
 
-// TODO Remove these comments
 /*
-Model URL: https://docs.openvinotoolkit.org/latest/_models_intel_human_pose_estimation_0001_description_human_pose_estimation_0001.html
-
-# Inputs
-
-Shape: [1x3x256x456]. An input image in the [BxCxHxW] format , where:
-    B - batch size
-    C - number of channels
-    H - image height
-    W - image width. Expected color order is BGR.
-
-# Outputs
-
-The net outputs two blobs with the [1, 38, 32, 57] and [1, 19, 32, 57] shapes. 
-The first blob contains keypoint pairwise relations (part affinity fields), 
-while the second blob contains keypoint heatmaps.
+Model URL: https://docs.openvinotoolkit.org/2019_R3.1/_models_intel_human_pose_estimation_0001_description_human_pose_estimation_0001.html
 */
 
 #include <string>
+#include <sstream>
 
 #include "vino_core_lib/models/human_pose_estimation_model.h"
 #include "vino_core_lib/slog.h"
@@ -60,34 +46,88 @@ void Models::HumanPoseEstimationModel::setLayerProperty(
 void Models::HumanPoseEstimationModel::checkLayerProperty(
     const InferenceEngine::CNNNetReader::Ptr& net_reader)
 {
-  slog::info << "Checking Age Gender Detection outputs" << slog::endl;
+  slog::info << "Checking Human Pose Estimation outputs" << slog::endl;
   InferenceEngine::OutputsDataMap output_info(
       net_reader->getNetwork().getOutputsInfo());
   auto it = output_info.begin();
-  InferenceEngine::DataPtr keypoints_output_ptr = (it++)->second;
+  InferenceEngine::DataPtr paf_output_ptr = (it++)->second;
   InferenceEngine::DataPtr heatmap_output_ptr = (it++)->second;
-  // output layer of age should be Convolution type
-  // TODO check if the output types are correct.
-//   if (heatmap_output_ptr->getCreatorLayer().lock()->type == "Convolution")
-//   {
-//     std::swap(keypoints_output_ptr, heatmap_output_ptr);
-//   }
-//   if (keypoints_output_ptr->getCreatorLayer().lock()->type != "Convolution")
-//   {
-//     throw std::logic_error("In Age Gender network, age layer (" +
-//                            keypoints_output_ptr->getCreatorLayer().lock()->name +
-//                            ") should be a Convolution, but was: " +
-//                            keypoints_output_ptr->getCreatorLayer().lock()->type);
-//   }
-//   if (heatmap_output_ptr->getCreatorLayer().lock()->type != "SoftMax")
-//   {
-//     throw std::logic_error("In Age Gender network, gender layer (" +
-//                            heatmap_output_ptr->getCreatorLayer().lock()->name +
-//                            ") should be a SoftMax, but was: " +
-//                            heatmap_output_ptr->getCreatorLayer().lock()->type);
-//   }
-  slog::info << "Keypoints layer: " 
-             << keypoints_output_ptr->getCreatorLayer().lock()->name
+  
+  if (paf_output_ptr->getCreatorLayer().lock()->type != "Convolution")
+  {
+    throw std::logic_error("In Human Pose Estimation network, PAF layer (" +
+                           paf_output_ptr->getCreatorLayer().lock()->name +
+                           ") should be a Convolution, but was: " +
+                           paf_output_ptr->getCreatorLayer().lock()->type);
+  }
+  if (heatmap_output_ptr->getCreatorLayer().lock()->type != "Convolution")
+  {
+    throw std::logic_error("In Human Pose Estimation network, heatmap layer (" +
+                           heatmap_output_ptr->getCreatorLayer().lock()->name +
+                           ") should be a Convolution, but was: " +
+                           heatmap_output_ptr->getCreatorLayer().lock()->type);
+  }
+
+  if (paf_output_ptr->getCreatorLayer().lock()->outData.size() != 1)
+  {
+    throw std::logic_error(
+      "In Human Pose Estimation network, PAF layer (" +
+      paf_output_ptr->getCreatorLayer().lock()->name +
+      ") should have 1 output, but had: " +
+      std::to_string(paf_output_ptr->getCreatorLayer().lock()->outData.size()));
+  }
+  if (heatmap_output_ptr->getCreatorLayer().lock()->outData.size() != 1)
+  {
+    throw std::logic_error(
+      "In Human Pose Estimation network, Heatmap layer (" +
+      heatmap_output_ptr->getCreatorLayer().lock()->name +
+      ") should have 1 output, but had: " +
+      std::to_string(heatmap_output_ptr->getCreatorLayer().lock()->outData.size()));
+  }
+
+  if (paf_output_ptr->getCreatorLayer().lock()->outData[0]->dims.size() == 4 && 
+      paf_output_ptr->getCreatorLayer().lock()->outData[0]->dims[2] == 19)
+  {
+    std::swap(paf_output_ptr, heatmap_output_ptr);
+  }
+
+  auto pafDims = paf_output_ptr->getCreatorLayer().lock()->outData[0]->dims;
+  auto heatDims = heatmap_output_ptr->getCreatorLayer().lock()->outData[0]->dims;
+
+  if (pafDims.size() != 4 || pafDims[0] != 57 || pafDims[1] != 32
+      || pafDims[2] != 38 || pafDims[3] != 1)
+  {
+    std::ostringstream size;
+    size << "[ ";
+    for (size_t s : pafDims)
+    {
+      size << s << " ";
+    }
+    size << "]";
+    throw std::logic_error(
+      "In Human Pose Estimation network, PAF layer (" +
+      paf_output_ptr->getCreatorLayer().lock()->name +
+      ") should have output size of [ 57 32 38 1 ], but had: " + size.str());
+  }
+
+  if (heatDims.size() != 4 || heatDims[0] != 57 || heatDims[1] != 32
+      || heatDims[2] != 19 || heatDims[3] != 1)
+  {
+    std::ostringstream size;
+    size << "[ ";
+    for (size_t s : heatDims)
+    {
+      size << s << " ";
+    }
+    size << "]";
+    throw std::logic_error(
+      "In Human Pose Estimation network, Heatmap layer (" +
+      heatmap_output_ptr->getCreatorLayer().lock()->name +
+      ") should have output size of [ 57 32 19 1 ], but had: " + size.str());
+  }
+
+  slog::info << "PAF layer: " 
+             << paf_output_ptr->getCreatorLayer().lock()->name
              << slog::endl;
   slog::info << "Heatmap layer: "
              << heatmap_output_ptr->getCreatorLayer().lock()->name 
