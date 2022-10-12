4921a24d7a8668041ab954be7142dc2b6f31a816
wujiawei <jiawei.wu@intel.com>
2022-10-12 12:44:42 +0800

    fix age gender detection pipeline

--- vino_core_lib/src/models/age_gender_detection_model.cpp
+++ vino_core_lib/src/models/age_gender_detection_model.cpp
@@ -30,34 +30,37 @@ Models::AgeGenderDetectionModel::AgeGenderDetectionModel(const std::string& labe
   : BaseModel(label_loc, model_loc, max_batch_size)
 {
 }
-bool Models::AgeGenderDetectionModel::updateLayerProperty(InferenceEngine::CNNNetwork& net_reader)
+bool Models::AgeGenderDetectionModel::updateLayerProperty(std::shared_ptr<ov::Model>& net_reader)
 {
   slog::info << "Checking INPUTs for model " << getModelName() << slog::endl;
   // set input property
-  InferenceEngine::InputsDataMap input_info_map(net_reader.getInputsInfo());
-  if (input_info_map.size() != 1)
-  {
-    slog::warn << "This model seems not Age-Gender-like, which should have only one input,"
-               << " but we got " << std::to_string(input_info_map.size()) << "inputs" << slog::endl;
+  inputs_info_ = net_reader->inputs();
+  ov::preprocess::PrePostProcessor ppp = ov::preprocess::PrePostProcessor(net_reader);
+  input_tensor_name_ = net_reader->input().get_any_name();
+  ov::preprocess::InputInfo& input_info = ppp.input(input_tensor_name_);
+
+  ov::Shape input_tensor_shape = net_reader->input().get_shape();
+  if (inputs_info_.size() != 1) {
+    slog::warn << "This model seems not Age-Gender-like, which should have only one input, but we got"
+      << std::to_string(input_tensor_shape.size()) << "inputs"
+      << slog::endl;
     return false;
   }
-  InferenceEngine::InputInfo::Ptr input_info = input_info_map.begin()->second;
-  input_info->setPrecision(InferenceEngine::Precision::FP32);
-  input_info->setLayout(InferenceEngine::Layout::NCHW);
-  addInputInfo("input", input_info_map.begin()->first);
+
+  addInputInfo("input", input_tensor_name_);
+  const ov::Layout tensor_layout{"NCHW"};
+  input_info.tensor().
+    set_element_type(ov::element::f32).
+    set_layout(tensor_layout);
+
   // set output property
-  InferenceEngine::OutputsDataMap output_info_map(net_reader.getOutputsInfo());
-  if (output_info_map.size() != 2)
-  {
-    // throw std::logic_error("Age/Gender Recognition network should have two output layers");
-    slog::warn << "This model seems not Age-gender like, which should have and only have 2"
-                  " outputs, but we got "
-               << std::to_string(output_info_map.size()) << "outputs" << slog::endl;
+  outputs_info_ = net_reader->outputs();
+  if (outputs_info_.size() != 2) {
+    slog::warn << "This model seems not Age-Gender-like, which should have and only have 2 outputs, but we got"
+      << std::to_string(outputs_info_.size()) << "outputs"
+      << slog::endl;
     return false;
   }
-  auto it = output_info_map.begin();
-  InferenceEngine::DataPtr age_output_ptr = (it++)->second;
-  InferenceEngine::DataPtr gender_output_ptr = (it++)->second;
 
 #if(0) ///
   // Check More Configuration:
@@ -81,17 +84,22 @@ bool Models::AgeGenderDetectionModel::updateLayerProperty(InferenceEngine::CNNNe
   slog::info << "Gender layer: " << gender_output_ptr->getCreatorLayer().lock()->name << slog::endl;
 #endif
 
+  auto age_output_info = outputs_info_[1];
+  ppp.output(age_output_info.get_any_name()).
+    tensor().
+    set_element_type(ov::element::f32).
+    set_layout(tensor_layout);
+  auto gender_output_info = outputs_info_[0];
+  ppp.output(gender_output_info.get_any_name()).
+    tensor().
+    set_element_type(ov::element::f32).
+    set_layout(tensor_layout);
 
-  age_output_ptr->setPrecision(InferenceEngine::Precision::FP32);
-  age_output_ptr->setLayout(InferenceEngine::Layout::NCHW);
-  gender_output_ptr->setPrecision(InferenceEngine::Precision::FP32);
-  gender_output_ptr->setLayout(InferenceEngine::Layout::NCHW);
+  net_reader = ppp.build();
+  ov::set_batch(net_reader_, getMaxBatchSize());
 
-  // output_age_ = age_output_ptr->name;
-  addOutputInfo("age", age_output_ptr->getName());
-  // output_gender_ = gender_output_ptr->name;
-  addOutputInfo("gender", gender_output_ptr->getName());
-
+  addOutputInfo("age", age_output_info.get_any_name());
+  addOutputInfo("gender", gender_output_info.get_any_name());
   printAttribute();
   return true;
 }
