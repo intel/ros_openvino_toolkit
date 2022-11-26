f4a0399fa06b58591d21ed83c4b96398486596c5
Corsair-cxs <1494089474@qq.com>
2022-11-27 00:37:39 +0800

    Add 'updateLayerProperty' function logic

--- vino_core_lib/src/models/face_detection_model.cpp
+++ vino_core_lib/src/models/face_detection_model.cpp
@@ -70,6 +70,61 @@ bool Models::FaceDetectionModel::fetchResults(const std::shared_ptr<Engines::Eng
 
 bool Models::FaceDetectionModel::updateLayerProperty(InferenceEngine::CNNNetwork& net_reader)
 {
+  slog::info << "Checking INPUTs for model " << getModelName() << slog::endl;
+
+  InferenceEngine::InputsDataMap input_info_map(net_reader.getInputsInfo());
+  if (input_info_map.size() != 1)
+  {
+    slog::warn << "This model seems not SSDNet-like, SSDnet has only one input, but we got "
+               << std::to_string(input_info_map.size()) << "inputs" << slog::endl;
+    return false;
+  }
+
+  InferenceEngine::InputInfo::Ptr input_info = input_info_map.begin()->second;
+  input_info->setPrecision(InferenceEngine::Precision::U8);
+  addInputInfo("input", input_info_map.begin()->first);
+
+  const InferenceEngine::SizeVector input_dims = input_info->getTensorDesc().getDims();
+  setInputHeight(input_dims[2]);
+  setInputWidth(input_dims[3]);
+
+  slog::info << "Checking OUTPUTs for model " << getModelName() << slog::endl;
+  InferenceEngine::OutputsDataMap output_info_map(net_reader.getOutputsInfo());
+  if (output_info_map.size() != 1)
+  {
+    slog::warn << "This model seems not SSDNet-like! We got " << std::to_string(output_info_map.size())
+               << "outputs, but SSDnet has only one." << slog::endl;
+    return false;
+  }
+  InferenceEngine::DataPtr& output_data_ptr = output_info_map.begin()->second;
+  addOutputInfo("output", output_info_map.begin()->first);
+  slog::info << "Checking Object Detection output ... Name=" << output_info_map.begin()->first << slog::endl;
+  output_data_ptr->setPrecision(InferenceEngine::Precision::FP32);
+
+  
+  // last dimension of output layer should be 7
+  const InferenceEngine::SizeVector output_dims = output_data_ptr->getTensorDesc().getDims();
+  setMaxProposalCount(static_cast<int>(output_dims[2]));
+  slog::info << "max proposal count is: " << getMaxProposalCount() << slog::endl;
+
+  auto object_size = static_cast<int>(output_dims[3]);
+  if (object_size != 7)
+  {
+    slog::warn << "This model is NOT SSDNet-like, whose output data for each detected object"
+               << "should have 7 dimensions, but was " << std::to_string(object_size) << slog::endl;
+    return false;
+  }
+  setObjectSize(object_size);
+
+  if (output_dims.size() != 4)
+  {
+    slog::warn << "This model is not SSDNet-like, output dimensions shoulld be 4, but was"
+               << std::to_string(output_dims.size()) << slog::endl;
+    return false;
+  }
+
+  printAttribute();
+  slog::info << "This model is SSDNet-like, Layer Property updated!" << slog::endl;
   return true;
 }
 
